3dd082b37f90e219d17e4a4ee142e607e61320b5
Tiag037 <d-tiago@live.fr>
2024-05-22 16:44:15 +0200

    preparation projet todolist et mise en place header

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./assets/components/Header";
+
+const App = () => {
+
+
+
+
+
+
+  
+  return (
+    <div>
+      <Header/>
+      <h1>sals</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,13 @@
+
+import "./Header.css";
+const Header = () => {
+
+
+    return (
+        <div className="header-container">
+            <h1 className="header-title">TodoList <span className="header-span">Made in Pipo</span></h1>
+        </div>
+    );
+};
+
+export default Header;
